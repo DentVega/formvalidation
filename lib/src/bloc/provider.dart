399877ee7a1cb40bfebb5155a8ce0ad8e65f900d
DentVega <dentvega6@gmail.com>
2020-01-05 21:38:26 -0400

    -InheritedWidget

---
+++ lib/src/bloc/provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:formvalidation/src/bloc/login_bloc.dart';
+
+class Provider extends InheritedWidget {
+
+  final logicBloc = LoginBloc();
+
+  Provider({Key key, Widget child}) : super(key: key, child: child);
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) {
+    // TODO: implement updateShouldNotify
+    return true;
+  }
+
+  static LoginBloc of (BuildContext context) {
+    return (context.inheritFromWidgetOfExactType(Provider) as Provider).logicBloc;
+  }
+
+}
