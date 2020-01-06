a7454c35cad4e96badd083613405e1b70c5e7533
DentVega <dentvega6@gmail.com>
2020-01-06 00:16:49 -0400

    - Validacion de formularios - FormWidget

--- lib/src/pages/producto_page.dart
+++ lib/src/pages/producto_page.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:formvalidation/src/utils/utils.dart' as utils;
 
-class ProductoPage extends StatelessWidget {
+class ProductoPage extends StatefulWidget {
+
+  @override
+  _ProductoPageState createState() => _ProductoPageState();
+}
+
+class _ProductoPageState extends State<ProductoPage> {
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +30,7 @@ class ProductoPage extends StatelessWidget {
         child: Container(
           padding: EdgeInsets.all(15.0),
           child: Form(
+            key: formKey,
               child: Column(
             children: <Widget>[
               _crearNombre(),
@@ -37,6 +47,13 @@ class ProductoPage extends StatelessWidget {
     return TextFormField(
       textCapitalization: TextCapitalization.sentences,
       decoration: InputDecoration(labelText: 'Producto'),
+      validator: (value) {
+        if (value.length < 3) {
+          return 'ingrese el nombre del producto';
+        } else {
+          return null;
+        }
+      },
     );
   }
 
@@ -44,19 +61,34 @@ class ProductoPage extends StatelessWidget {
     return TextFormField(
       keyboardType: TextInputType.numberWithOptions(decimal: true),
       decoration: InputDecoration(labelText: 'Precio'),
+      validator: (value) {
+        if (utils.isNumeric(value)) {
+          return null;
+        } else {
+          return 'Sólo númereros';
+        }
+      },
     );
   }
 
   Widget _crearBoton() {
     return RaisedButton.icon(
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20.0)
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
       icon: Icon(Icons.save),
       label: Text('Guardar'),
       color: Colors.deepPurple,
       textColor: Colors.white,
-      onPressed: () {},
+      onPressed: _submit,
     );
   }
+
+  void _submit() {
+    if (formKey.currentState.validate()) {
+      //Si el formulario es valido
+      print('Todo ok');
+    }  else {
+      //Si el formulario no es valido
+    }
+  }
+
 }
